6e3769cc17e628826ec4aade0bc4cb4e019b361a
zadumka <90329284+zadumka@users.noreply.github.com>
2024-04-08 14:33:44 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -16,12 +16,5 @@ profile.username = newName;
 }
 
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
 
